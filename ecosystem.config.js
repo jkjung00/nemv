126fd89eb4cfa2e4adde8b70cd77ee9cbcbedd87
jkjung00 <jkjung00@naver.com>
2020-08-29 02:17:26 +0900

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,22 +7,9 @@ module.exports = {
       NODE_ENV: "development",
       port: 3000
     },
-    env_pro: {
+    env_pr: {
       NODE_ENV: "production",
       port: 80
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  }]
 };
